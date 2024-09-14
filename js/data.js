69675a5bf6b8107a5c16534ca6c73156fb78907a
FerCastilloRdz <f.castillorodriguez@ugto.mx>
2024-09-13 18:00:31 -0600

    Reproduce musica

---
+++ js/data.js
@@ -0,0 +1,93 @@
+//datos del song y artist
+let songs = [
+    {
+      name: 'Song 1',
+      artist: 'Artist 1',
+      path: '/assets/musics/Song1.mp3',
+      cover: '/assets/images/cover1.png'
+    },
+    {
+      name: 'Song 2',
+      artist: 'Artist 2',
+      path: '/assets/musics/Song2.mp3',
+      cover: '/assets/images/cover2.png'
+    },
+    {
+      name: 'Song 3',
+      artist: 'Artist 3',
+      path: '/assets/musics/Song3.mp3',
+      cover: '/assets/images/cover3.png'
+    },
+    {
+      name: 'Song 4',
+      artist: 'Artist 4',
+      path: '/assets/musics/Song4.mp3',
+      cover: '/assets/images/cover4.png'
+    },
+    {
+      name: 'Song 5',
+      artist: 'Artist 5',
+      path: '/assets/musics/Song5.mp3',
+      cover: '/assets/images/cover5.png'
+    },
+    {
+        name: 'Song 6',
+        artist: 'Artist 6',
+        path: '/assets/musics/Song6.mp3',
+        cover: '/assets/images/cover1.png'
+      },
+      {
+        name: 'Song 7',
+        artist: 'Artist 7',
+        path: '/assets/musics/Song7.mp3',
+        cover: '/assets/images/cover2.png'
+      },
+      {
+        name: 'Song 8',
+        artist: 'Artist 8',
+        path: '/assets/musics/Song8.mp3',
+        cover: '/assets/images/cover3.png'
+      },
+      {
+        name: 'Song 9',
+        artist: 'Artist 9',
+        path: '/assets/musics/Song9.mp3',
+        cover: '/assets/images/cover4.png'
+      },
+      {
+        name: 'Song 10',
+        artist: 'Artist 10',
+        path: '/assets/musics/Song10.mp3',
+        cover: '/assets/images/cover5.png'
+      },
+      {
+        name: 'Song 11',
+        artist: 'Artist 11',
+        path: '/assets/musics/Song11.mp3',
+        cover: '/assets/images/cover1.png'
+      },
+      {
+        name: 'Song 12',
+        artist: 'Artist 12',
+        path: '/assets/musics/Song12.mp3',
+        cover: '/assets/images/cover2.png'
+      },
+      {
+        name: 'Song 13',
+        artist: 'Artist 13',
+        path: '/assets/musics/Song13.mp3',
+        cover: '/assets/images/cover3.png'
+      },
+      {
+        name: 'Song 14',
+        artist: 'Artist 14',
+        path: '/assets/musics/Song14.mp3',
+        cover: '/assets/images/cover4.png'
+      },
+      {
+        name: 'Song 15',
+        artist: 'Artist 15',
+        path: '/assets/musics/Song15.mp3',
+        cover: '/assets/images/cover5.png'
+      }
+  ]
